04ab0d5fa8255413d2f03a59f3f5fd5aa9449d35
Federico De Laurentiis <federicodelaurentiis7@gmail.com>
2018-06-14 11:58:55 +0200

    Update qavmentorAPI.py

--- qavmentorAPI.py
+++ qavmentorAPI.py
@@ -6,11 +6,8 @@ import requests
 from utils.r_model_predictor import RModelPredictor 
 from flask_cors import CORS
 
-<<<<<<< HEAD
 tipsHandler=TipsHandler()
-=======
 
->>>>>>> eeed76a935a67c31137bf7eb150519fb1c6b7633
 app = Flask(__name__)
 CORS(app)
 
@@ -27,22 +24,15 @@ def analyze():
 
 @app.route('/getTip', methods=['POST'])
 def getTip():
-<<<<<<< HEAD
 	feature_extractor = FeatureAnalysis(request.get_json())
 	features = feature_extractor.extractFeatures()
 	tipsHandler.choseTips(features)
 	print(tipsHandler.choseTips(feature_extractor))
 	return json.dumps([{'msg':'Lorem ipsum dolor sit amet, qui officia deserunt mollit anim id est laborum.', 'tip_index':1},{'msg':'msg2', 'tip_index':2},{'msg':'msg3', 'tip_index':3}])
-=======
-    return json.dumps([{'msg':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'tip_index':1},{'msg':'msg2', 'tip_index':2},{'msg':'msg3', 'tip_index':3}])
->>>>>>> eeed76a935a67c31137bf7eb150519fb1c6b7633
 
 
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
 	app.run(debug=True)
-=======
-    app.run(debug=True)
->>>>>>> eeed76a935a67c31137bf7eb150519fb1c6b7633
+
