8a46ce80c6ab6261198ab27c42200defd1c8a9b6
gigium <gigiM1955@gmail.com>
2018-06-24 12:57:39 +0200

    test continous deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,8 +5,8 @@ chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
 git config --global push.default matching
-echo deploying
-git remote add deploy ssh://root@$IP:$PORT$DEPLOY_DIR
+echo ____________________deploying____________________
+git remote add deploy $REPO_URI
 git push deploy master
 
 # Skip this command if you don't need to execute any additional commands after deploying.
