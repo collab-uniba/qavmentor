b5736bd27717874c378c9b49b62744bb84429639
federico-code <federicodelaurentiis7@gmail.com>
2018-06-29 18:40:06 +0200

    updating

--- R_service/server.R
+++ R_service/server.R
@@ -1,5 +1,4 @@
 library(plumber)
-#api <- plumb("/var/www/qavmentor/R_service/api.R")
-api <- plumb(paste(getwd(),"api.r",sep="/"))
-#print(getwd())
+api <- plumb("/var/www/qavmentor/R_service/api.R")
+#api <- plumb(paste(getwd(),"api.r",sep="/")) for testing on local host
 api$run(port=1111)
