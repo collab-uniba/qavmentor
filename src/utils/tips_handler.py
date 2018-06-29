5e8a90a0c28c31c70b1fbfc0d98d171b54b932dc
gigium <gigiM1995@gmail.com>
2018-06-29 16:36:15 +0200

    Update tips_handler.py

--- src/utils/tips_handler.py
+++ src/utils/tips_handler.py
@@ -13,7 +13,6 @@ class TipsHandler:
 	def __init__(self,tipsFileName="tipsFile.json"):
 		in_file=open(os.path.dirname(os.path.abspath(__file__))+'/'+tipsFileName,"r")
 		self.__tips=json.loads(in_file.read())
-		print(self.__tips)
 		in_file.close()
 	
 
@@ -24,9 +23,9 @@ class TipsHandler:
 
 		#cycle for tips
 		for tip in self.__tips:
-			if tip["category"] in category:
-				problem=tip["problem"]
-				for key,value in features.items():
-					if key in problem and value==problem[key]:
-						chosenTips.append(tip)
+			#if tip["category"] in category:
+			problem=tip["problem"]
+			for key,value in features.items():
+				if key in problem and value==problem[key]:
+					chosenTips.append(tip)
 		return chosenTips
