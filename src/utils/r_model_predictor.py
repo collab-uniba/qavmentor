a1b0e64d33d16b89fd4d8c36ed68ac4cfa7878a5
gigium <gigiM1955@gmail.com>
2018-07-02 11:48:45 +0200

    adding reputation based prediction

--- src/utils/r_model_predictor.py
+++ src/utils/r_model_predictor.py
@@ -6,6 +6,7 @@ import json
 
 class RModelPredictor:
 	def __init__(self, features):
+		self.__maxScoreByReputation = {"New":28,"Low":48,"Established":51, "Trusted":55}
 		self.__data = {"UserReputation": features["UserReputation"], "CodeSnippet": features["CodeSnippet"], "Weekday": features['Weekday'],
 		 "GMTHour": features['GMTHour'], "BodyLength": features['BodyLength'], "TitleLength": features['TitleLength'],
 		  "URL": features['URL'], "AvgUpperCharsPPost": features['AvgUpperCharsPPost'], "SentimentPositiveScore": features['SentimentPositiveScore'],
@@ -16,8 +17,12 @@ class RModelPredictor:
 		start_time = time.time()
 		r = requests.post("http://127.0.0.1:1111/model_predict",data=json.dumps(self.__data))
 		print("_______________ %s seconds _______________" % (time.time() - start_time))		  
-
-		return float((r.text).replace("[", "").replace("]", ""))
+		prediction = float((r.text).replace("[", "").replace("]", ""))
+		predictionByReputation = (prediction*100)/self.__maxScoreByReputation[self.__data['UserReputation']]
+		if predictionByReputation < 100:
+			return predictionByReputation
+		else:
+			return 100
 		'''
 		proc = subprocess.Popen(['Rscript','predict.R'] + self.__data , stdout=subprocess.PIPE)
 		output = proc.stdout.read()
