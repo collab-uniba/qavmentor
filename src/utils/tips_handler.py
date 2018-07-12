df8293300f95bc770d9d21ecf39894a242db24d7
federico-code <federicodelaurentiis7@gmail.com>
2018-07-12 17:35:24 +0200

    Categorization of tips in actionable and not actionable

--- src/utils/tips_handler.py
+++ src/utils/tips_handler.py
@@ -1,13 +1,6 @@
 import os
 import json
-''' request example
-	  "day": (date.getDay()).toString(),
-      "hour": (date.getHours()).toString(),
-      "body": html_question_inner,
-      "title":title.value,
-      "tags": tag.value.split(" ")
-      "modified": ["title","body","tag"]
-'''
+
 
 class TipsHandler:
 	def __init__(self,tipsFileName="tipsFile.json"):
@@ -20,8 +13,6 @@ class TipsHandler:
 	def choseTips(self,features):
 		chosenTips=[]
 		
-
-		#cycle trough tips
 		for tip in self.__tips:
 			#if tip["category"] in category:
 			problem=tip["problem"]
