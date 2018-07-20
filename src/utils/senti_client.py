f8664d26d08897e8a8d9bf55e860b1e1572fc7a8
federico-code <federicodelaurentiis7@gmail.com>
2018-07-20 12:37:22 +0200

    increase senti_client code efficiency

--- src/utils/senti_client.py
+++ src/utils/senti_client.py
@@ -18,16 +18,8 @@ class sentistrength():
         self.port = port 
         self.address = address
 
-    def __del__(self):
-        if self.sentistrength:
-            os.killpg(self.sentistrength.pid,15)
-
-    def run_server(self, language):
-        if language!=self.language and self.sentistrength:
-            os.killpg(self.sentistrength.pid,15)
-            time.sleep(1)
+    def get_socket(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
         sock.connect((self.address,self.port))
         return sock
     
@@ -35,7 +27,7 @@ class sentistrength():
     def get_sentiment(self, string_to_code, language="EN"):
         url_encoded = urllib.parse.quote(string_to_code)
         request_string = "GET /%s HTTP/1.0 \r\n\r\n" %url_encoded
-        sock = self.run_server(language)
+        sock = self.get_socket()
         sock.sendall(str.encode(request_string,'UTF-8'))
         response = sock.recv(4096)
         resp_string = response.decode()
